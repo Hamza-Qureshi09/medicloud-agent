9a21f5351c0ca5ff55b8694c69eaba30189593d6
Zulfiqar Haider <zulfiqarh61@gmail.com>
2026-08-31 21:05:26 +0500

    request will be sent every 25 seconds

--- frontend/src/pages/control/index.tsx
+++ frontend/src/pages/control/index.tsx
@@ -14,6 +14,7 @@ import { api } from "@/lib/api";
 export function ControlPage() {
     const { data, error } = useSWR(api.agent.slavesKey, api.agent.slaves, {
         revalidateOnFocus: false,
+        refreshInterval: 25000,
     });
 
     // If API fails, fallback to static data. Otherwise use fetched data (or static while loading initially).
@@ -31,7 +32,7 @@ export function ControlPage() {
             return acc;
         }
     }, 0);
-
+    
     return (
         <Container>
             <PageSection
